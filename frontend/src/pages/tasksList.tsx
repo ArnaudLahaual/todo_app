c362c78ad333815a53c3ac99aa51aa1239393671
ArnaudLahaual <arnaud.lahaual@gmail.com>
2026-04-01 00:16:05 +0200

    bug compil front vite nodemon

--- frontend/src/pages/tasksList.tsx
+++ frontend/src/pages/tasksList.tsx
@@ -1,7 +1,35 @@
-export default function tasksList() {
+import { useEffect, useState } from "react";
+import type { Task } from "../../../backend/src/types/task";
+import axios from "axios";
+
+export default function TasksList() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleGetTasks = () => {
+    axios
+      .get("http://localhost:3000/api/tasks/")
+      .then((resp) => {
+        setTasks(resp.data);
+      })
+      .catch((err) => {
+        console.error("Erreur fetch tasks :", err);
+      });
+  };
+
+  useEffect(() => {
+    handleGetTasks();
+  }, []);
+
   return (
     <div>
-      <h1>Liste des tâches</h1>
+      <h1>Liste des tâchessssss</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}-{task.done ? "Faite" : "Pas faite"}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
